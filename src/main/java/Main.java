b05fedcefa7ea2e05011190e0434db9b31b5c7d7
Thien Truong <tcthien@gmail.com>
2019-03-07 08:30:07 -0500

    simplified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,65 @@
-import com.fasterxml.jackson.core.JsonEncoding;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
+import org.apache.http.Header;
+import org.apache.http.HttpMessage;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.FileEntity;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.testng.Assert;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String json = "{\n" +
+                "\t\"name\": \"Thien Truong\",\n" +
+                "\t\"email\": \"tcthien@gmail.com\",\n" +
+                "\t\"position\": \"Software Developer in Test\",\n" +
+                "\t\"urls\": [\"https://my.indeed.com/r/Thien-Truong/3f347064eb13c0fd/byId/Eek-r6BK24a3Kvf3GB6Mxg/pdf\", \"https://www.linkedin.com/in/thientruong\", \"https://github.com/tcthien00/FlightAware\"],\n" +
+                "\t\"comment\": \"Please contact me for more information\"\n" +
+                "}";
+      StringEntity entity = new StringEntity(json);
 
-        // prepare json file
-        File myFile = new File("ThienTruongFlightAware.json");
-
-        JsonFactory jsonFactory = new JsonFactory();
-        JsonGenerator jsonGenerator = jsonFactory.createGenerator(myFile, JsonEncoding.UTF8);
-        jsonGenerator.writeStartObject();
-
-        jsonGenerator.writeStringField("name", "Thien Truong");
-        jsonGenerator.writeStringField("email", "tcthien@gmail.com");
-        jsonGenerator.writeStringField("position", "Software Developer in Test");
-
-        jsonGenerator.writeFieldName("urls");
-        jsonGenerator.writeStartArray();
-        jsonGenerator.writeString("https://my.indeed.com/r/Thien-Truong/3f347064eb13c0fd/byId/Eek-r6BK24a3Kvf3GB6Mxg/pdf");
-        jsonGenerator.writeString("https://www.linkedin.com/in/thientruong");
-        jsonGenerator.writeString("https://github.com/tcthien00/FlightAware");
-        jsonGenerator.writeEndArray();
-
-        jsonGenerator.writeStringField("comment", "Please contact me for more information");
-
-        jsonGenerator.writeEndObject();
-        jsonGenerator.close();
-
-        // submit application
-        CloseableHttpClient client = HttpClients.createDefault();
+        // prepare POST
         HttpPost httpPost = new HttpPost("https://flightaware.com/about/careers/apply?data=");
-
-        FileEntity entity = new FileEntity(myFile);
+        httpPost.setHeader("Content-Type", "application/json");
         httpPost.setEntity(entity);
-        httpPost.setHeader("Accept", "application/json");
-        httpPost.setHeader("Content-type", "application/json");
+        printMessageHeaders(httpPost);
 
+        // send POST to submit application
+        CloseableHttpClient client = HttpClients.createDefault();
         CloseableHttpResponse response = client.execute(httpPost);
+        printMessageHeaders(response);
+
+        //read the response code
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY){
+            String newLoc = response.getHeaders("Location").toString();
+            System.out.println(newLoc);
+            httpPost.setURI(URI.create(newLoc));
+            response = client.execute(httpPost);
+            printMessageHeaders(response);
+        }
+
+        // verify response code
         Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
         client.close();
+    }
+
+    public static void printMessageHeaders(HttpMessage message) {
+        Header[] headers = message.getAllHeaders();
+        System.out.println("\n------------------------");
+        if (message instanceof HttpUriRequest)
+            System.out.println(((HttpUriRequest) message).getRequestLine());
+        else
+            System.out.println(((HttpResponse) message).getStatusLine());
+        for (Header h : headers) {
+            System.out.println(h.getName() + ": " + h.getValue());
+        }
+        System.out.println("------------------------");
     }
 }
